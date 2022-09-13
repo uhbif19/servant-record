1de63d917b500ee9c4614a28ca8e33523a66663a
uhbif19 <uhbif19@gmail.com>
2022-09-13 19:24:21 +0300

    Move to Formolou formatting

--- src/Servant/Record.hs
+++ src/Servant/Record.hs
@@ -9,96 +9,99 @@ import Data.Proxy (Proxy (Proxy))
 import Data.Text.Encoding (encodeUtf8)
 import Data.Typeable (typeRep)
 import GHC.Generics (Generic (..))
+
 import Network.HTTP.Types.URI (queryToQueryText, urlEncode)
 import Network.Wai (queryString)
+
 import Servant.API ((:>))
 import Servant.Client.Core (HasClient (..))
 import Servant.Client.Core.Request (appendToQueryString)
 import Servant.Links (HasLink (..))
-import Servant.Reflection (RecordReflectionRep (..))
 import Servant.Server (HasServer (..))
 import Servant.Server.Internal.Context (HasContextEntry, getContextEntry)
 import Servant.Server.Internal.Delayed (addParameterCheck)
 import Servant.Server.Internal.DelayedIO (delayedFailFatal, withRequest)
-import Servant.Server.Internal.ErrorFormatter
-  ( ErrorFormatters,
+import Servant.Server.Internal.ErrorFormatter (
+    ErrorFormatters,
     MkContextWithErrorFormatter,
     mkContextWithErrorFormatter,
     urlParseErrorFormatter,
-  )
+ )
+
+import Servant.Reflection (RecordReflectionRep (..))
 
 data QueryParamsRecord (record :: Type)
 
 instance
-  ( Generic record,
-    RecordReflectionRep (Rep record),
-    HasServer api context,
-    HasContextEntry (MkContextWithErrorFormatter context) ErrorFormatters
-  ) =>
-  HasServer (QueryParamsRecord record :> api) context
-  where
-  type ServerT (QueryParamsRecord record :> api) m = record -> ServerT api m
-
-  hoistServerWithContext _ context transformer server =
-    hoistServerWithContext (Proxy :: Proxy api) context transformer . server
-
-  route _ context subserver =
-    route (Proxy :: Proxy api) context $
-      subserver `addParameterCheck` withRequest getRecord
+    ( Generic record
+    , RecordReflectionRep (Rep record)
+    , HasServer api context
+    , HasContextEntry (MkContextWithErrorFormatter context) ErrorFormatters
+    ) =>
+    HasServer (QueryParamsRecord record :> api) context
     where
-      rep = typeRep (Proxy :: Proxy QueryParamsRecord)
-      formatError =
-        urlParseErrorFormatter $
-          getContextEntry (mkContextWithErrorFormatter context)
-      queryFailed req =
-        delayedFailFatal $
-          formatError rep req $
-            "Query parameters are wrong"
-      queryparams req =
-        fromList
-          [ (key, value)
-            | (key, Just value) <- (queryToQueryText . queryString) req
-          ]
-      getRecord req = case fromMapRep $ queryparams req of
-        Just recordRep -> return $ to recordRep
-        Nothing -> queryFailed req
+    type ServerT (QueryParamsRecord record :> api) m = record -> ServerT api m
+
+    hoistServerWithContext _ context transformer server =
+        hoistServerWithContext (Proxy :: Proxy api) context transformer . server
+
+    route _ context subserver =
+        route (Proxy :: Proxy api) context $
+            subserver `addParameterCheck` withRequest getRecord
+      where
+        rep = typeRep (Proxy :: Proxy QueryParamsRecord)
+        formatError =
+            urlParseErrorFormatter $
+                getContextEntry (mkContextWithErrorFormatter context)
+        queryFailed req =
+            delayedFailFatal $
+                formatError rep req $
+                    "Query parameters are wrong"
+        queryparams req =
+            fromList
+                [ (key, value)
+                | (key, Just value) <- (queryToQueryText . queryString) req
+                ]
+        getRecord req = case fromMapRep $ queryparams req of
+            Just recordRep -> return $ to recordRep
+            Nothing -> queryFailed req
 
 instance
-  ( Generic record,
-    RecordReflectionRep (Rep record),
-    HasLink endpoint
-  ) =>
-  HasLink (QueryParamsRecord record :> endpoint)
-  where
-  type
-    MkLink (QueryParamsRecord record :> endpoint) a =
-      record -> MkLink endpoint a
+    ( Generic record
+    , RecordReflectionRep (Rep record)
+    , HasLink endpoint
+    ) =>
+    HasLink (QueryParamsRecord record :> endpoint)
+    where
+    type
+        MkLink (QueryParamsRecord record :> endpoint) a =
+            record -> MkLink endpoint a
 
-  -- Current solution does nothing cuz of
-  -- https://github.com/haskell-servant/servant/issues/1232
-  toLink toA _ link _record = toLink toA (Proxy :: Proxy endpoint) link
+    -- Current solution does nothing cuz of
+    -- https://github.com/haskell-servant/servant/issues/1232
+    toLink toA _ link _record = toLink toA (Proxy :: Proxy endpoint) link
 
 instance
-  ( Generic record,
-    RecordReflectionRep (Rep record),
-    HasClient m endpoint
-  ) =>
-  HasClient m (QueryParamsRecord record :> endpoint)
-  where
-  type
-    Client m (QueryParamsRecord record :> endpoint) =
-      record -> Client m endpoint
-
-  hoistClientMonad pm _ f cl =
-    hoistClientMonad pm (Proxy :: Proxy endpoint) f . cl
-
-  clientWithRoute pm _ req record =
-    clientWithRoute pm (Proxy :: Proxy endpoint) reqWithParams
+    ( Generic record
+    , RecordReflectionRep (Rep record)
+    , HasClient m endpoint
+    ) =>
+    HasClient m (QueryParamsRecord record :> endpoint)
     where
-      kvlist = toList . toMapRep . from
-      addParam req_ (k, v) =
-        appendToQueryString
-          k
-          ((Just . urlEncode True . encodeUtf8) v)
-          req_
-      reqWithParams = foldl' addParam req (kvlist record)
+    type
+        Client m (QueryParamsRecord record :> endpoint) =
+            record -> Client m endpoint
+
+    hoistClientMonad pm _ f cl =
+        hoistClientMonad pm (Proxy :: Proxy endpoint) f . cl
+
+    clientWithRoute pm _ req record =
+        clientWithRoute pm (Proxy :: Proxy endpoint) reqWithParams
+      where
+        kvlist = toList . toMapRep . from
+        addParam req_ (k, v) =
+            appendToQueryString
+                k
+                ((Just . urlEncode True . encodeUtf8) v)
+                req_
+        reqWithParams = foldl' addParam req (kvlist record)
--- tests/Main.hs
+++ tests/Main.hs
@@ -14,7 +14,7 @@ import Servant.Server (Application, ErrorFormatter, Handler, Server, errBody, er
 import Test.Hspec (describe, hspec, it, shouldBe)
 
 data TestRecord = TestConstructor {testField :: String}
-  deriving (Generic, Show, Eq)
+    deriving (Generic, Show, Eq)
 
 type TestAPI = "test" :> QueryParamsRecord TestRecord :> Get '[JSON] String
 
@@ -22,23 +22,23 @@ testApiProxy :: Proxy TestAPI
 testApiProxy = Proxy
 
 handler :: TestRecord -> Handler String
-handler TestConstructor {testField} = return $ "Returning " ++ testField
+handler TestConstructor{testField} = return $ "Returning " ++ testField
 
 testGet = client testApiProxy
 
 main = do
-  hspec $ do
-    describe "Servant Record" $ do
-      it "Reflection working" $ do
-        let record = TestConstructor "test"
-        let roundtrip = fmap to . fromMapRep . toMapRep . from
-        roundtrip record `shouldBe` Just record
-      it "API working" $ do
-        serverThreadId <- forkIO $ run 3000 $ serve testApiProxy handler
-        manager <- newManager defaultManagerSettings
-        url <- parseBaseUrl "http://localhost:3000"
-        let env = mkClientEnv manager url
-        result <-
-          runClientM (testGet $ TestConstructor "test value") env
-        result `shouldBe` Right "Returning test value"
-        killThread serverThreadId
+    hspec $ do
+        describe "Servant Record" $ do
+            it "Reflection working" $ do
+                let record = TestConstructor "test"
+                let roundtrip = fmap to . fromMapRep . toMapRep . from
+                roundtrip record `shouldBe` Just record
+            it "API working" $ do
+                serverThreadId <- forkIO $ run 3000 $ serve testApiProxy handler
+                manager <- newManager defaultManagerSettings
+                url <- parseBaseUrl "http://localhost:3000"
+                let env = mkClientEnv manager url
+                result <-
+                    runClientM (testGet $ TestConstructor "test value") env
+                result `shouldBe` Right "Returning test value"
+                killThread serverThreadId
